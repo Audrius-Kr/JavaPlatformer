fc50bba620a7ad31434c5d7e7f6eed03a1334ab7
Audrius Kralikas <audrius.kralikas@mif.stud.vu.lt>
2024-02-20 13:44:00 +0200

    Veikia background

--- core/src/screens/GameScreen.java
+++ core/src/screens/GameScreen.java
@@ -8,8 +8,11 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapImageLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
@@ -28,6 +31,7 @@ import sprites.Player;
 
 public class GameScreen implements Screen {
 
+    private final int BCGRD_LAYER_INDEX = 0;
     private OrthographicCamera gameCamera;
     private Viewport gamePort;
     private CatGame game;
@@ -39,7 +43,11 @@ public class GameScreen implements Screen {
     private World world;
     private Box2DDebugRenderer b2dr;
     private Player player;
+    TiledMapImageLayer backgroundLayer;
+    TextureRegion background;
     private float deadTimer = 0;
+    int repeatY;
+    int repeatX;
 
 
 
@@ -57,6 +65,11 @@ public class GameScreen implements Screen {
         player = new Player(world, this, 100, 100);
         creator = new B2WorldCreator(this);
         world.setContactListener(new WorldListener());
+        backgroundLayer = (TiledMapImageLayer) map.getLayers().get(BCGRD_LAYER_INDEX);
+        background = backgroundLayer.getTextureRegion();
+        repeatX = (int) Math.ceil(Gdx.graphics.getWidth() / (float) (background.getRegionWidth() / CatGame.PPM));
+        repeatY = (int) Math.ceil(Gdx.graphics.getHeight() / (float) background.getRegionHeight());
+
 
 
 
@@ -103,16 +116,21 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
-
         update(delta);
+
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        renderer.render();
-        game.batch.setProjectionMatrix(gameCamera.combined);
-        b2dr.render(world, gameCamera.combined);
+
         game.batch.setProjectionMatrix(gameCamera.combined);
         game.batch.begin();
-        player.draw(game.batch);
+
+
+        for (int x = 0; x < repeatX; x++) {
+
+                game.batch.draw(background, x * background.getRegionWidth() / CatGame.PPM, 0, background.getRegionWidth() / CatGame.PPM, background.getRegionHeight() / CatGame.PPM);
+
+        }
+
         for (FinalCat entity : creator.getFinalCats()) {
             idleFrame = entity.getIdleFrame();
             float x = entity.getX();
@@ -121,7 +139,13 @@ public class GameScreen implements Screen {
             float h = idleFrame.getRegionHeight();
             game.batch.draw(idleFrame, x, y, w / CatGame.PPM, h / CatGame.PPM);
         }
+        renderer.render();
+        player.draw(game.batch);
         game.batch.end();
+
+
+        b2dr.render(world, gameCamera.combined);
+
         if (player.dead)
             deadTimer += delta;
         if (deadTimer >= 3) {
